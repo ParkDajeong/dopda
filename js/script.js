c7d6b9772b07518751e3d1f2ed1789aa9a3dc30a
ParkDajeong <sobeast980@gmail.com>
2021-01-27 11:56:27 +0900

    모바일 nav 스크립트 작성

--- js/script.js
+++ js/script.js
@@ -32,3 +32,17 @@ $(function() {
     pauseOnFocus: false,
   });
 });
+
+// 모바일 nav 
+$(function() {
+  $("header .open").on("click", function() {
+    $("body").css({"overflow": "hidden"});
+    $("header .bg").css({"display": "block"});
+    $("header nav").addClass("on");
+  });
+  $("header .close, header .bg").on("click", function() {
+    $("body").css({"overflow": "auto"});
+    $("header .bg").css({"display": "none"});
+    $("header nav").removeClass("on");
+  });
+});
